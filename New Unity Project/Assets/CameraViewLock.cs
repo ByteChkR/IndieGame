c17cdfbe99634c38ea7ba5662d943b4523bb7d4f
Tim Akermann <ti.akermann@gmail.com>
2018-11-19 20:47:27 +0100

    Movement Implemented. DIfferent Strafe/Forwards/Backwards Speeds, Mouse Controls.

---
+++ New Unity Project/Assets/CameraViewLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewLock : MonoBehaviour {
+
+    public Transform target;
+    Vector3 distance;
+	// Use this for initialization
+	void Start () {
+        distance = transform.position - target.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = target.position + distance;
+	}
+}
---
+++ New Unity Project/Assets/Controller.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class Controller : MonoBehaviour
+{
+    public Camera c;
+
+    public KeyCode Forward;
+    public KeyCode Backward;
+    public KeyCode Left;
+    public KeyCode Right;
+    public float ForwardSpeed = 1;
+    public float BackwardSpeed = 0.5f;
+    public float StrafeSpeed = 0.75f;
+    public float StrafeCutoff = 0.2f;
+
+    private Rigidbody _rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 vDir = ViewingDirection();
+
+        transform.forward = new Vector3(vDir.x, 0, vDir.z);
+        Debug.DrawRay(transform.position, vDir, Color.blue);
+
+
+        float speed;
+        if (_rb.velocity == Vector3.zero)
+        {
+            {
+                speed = ForwardSpeed;
+                Debug.Log("Not Moving");
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, _rb.velocity, Color.red);
+            vDir.y = 0;
+            float d = Vector3.Dot(vDir.normalized, _rb.velocity.normalized);
+            Debug.Log(d);
+            if (d < StrafeCutoff && d > -StrafeCutoff)
+            {
+                speed = StrafeSpeed;
+                Debug.Log("Strafing");
+            }
+            else if (d < -StrafeCutoff)
+            {
+                Debug.Log("Backwards");
+                speed = BackwardSpeed;
+            }
+            else
+            {
+                Debug.Log("Forward");
+                speed = ForwardSpeed;
+            }
+        }
+        
+        if (Input.GetKey(Forward))
+        {
+            _rb.AddForce(Vector3.forward* speed, ForceMode.Acceleration);
+        }
+        else if (Input.GetKey(Backward))
+        {
+            _rb.AddForce(Vector3.back * speed, ForceMode.Acceleration);
+        }
+        else if (Input.GetKey(Left))
+        {
+            _rb.AddForce(Vector3.left* speed, ForceMode.Acceleration);
+        }
+        else if (Input.GetKey(Right))
+        {
+            _rb.AddForce(Vector3.right * speed, ForceMode.Acceleration);
+        }
+        
+    }
+
+    Vector3 ViewingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray r = c.ScreenPointToRay(mousePos);
+        RaycastHit info;
+        if(Physics.Raycast(r, out info, 1000, 1 << 9))
+        {
+            return info.point-transform.position;
+        }
+        return -Vector3.one;
+    }
+
+}
