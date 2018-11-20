69854e3aa5a5d3bc19e339678ed995e679776999
Tim Akermann <ti.akermann@gmail.com>
2018-11-20 09:53:35 +0100

    Added basic architechture to support a wide range of abilities.

---
+++ New Unity Project/Assets/AbstractAbility.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class AbstractAbilityInstance : MonoBehaviour
+{
+    Dictionary<int, bool> players = new Dictionary<int, bool>();
+    int source;
+
+    public void RegisterSource(int dummy)
+    {
+        source = dummy;
+    }
+
+    public void RegisterPlayer(int dummy)
+    {
+        if (players.ContainsKey(dummy)) players[dummy] = true;
+        else players.Add(dummy, true);
+    }
+
+    public void UnregisterPlayer(int dummy)
+    {
+        players[dummy] = false;
+    }
+}
+
+public abstract class AbstractAbility : MonoBehaviour
+{
+
+    public string Name;
+    public string Description;
+    public Sprite icon;
+    public AbstractAbilityInstance abilityInstance;
+
+    public virtual void Fire(int dummy)
+    {
+        AbstractAbilityInstance a = Instantiate(abilityInstance);
+        a.RegisterSource(dummy);
+    }
+
+}
---
+++ New Unity Project/Assets/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Weapon : MonoBehaviour {
+
+    public int owner;
+    public List<AbstractAbility> abilities;
+    public List<KeyCode> abilityKeyBindings;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < abilityKeyBindings.Count; i++)
+        {
+            if (Input.GetKeyDown(abilityKeyBindings[i]))
+            {
+                abilities[i].Fire(owner);
+            }
+        }
+	}
+}
