68870696980d1456cdc00f02f9b773fc531c04bc
Tim Akermann <ti.akermann@gmail.com>
2018-11-22 11:22:22 +0100

    made the coin move towards the player

--- New Unity Project/Assets/Coin.cs
+++ New Unity Project/Assets/Coin.cs
@@ -2,35 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Coin : MonoBehaviour
+{
 
     public float startSpeed;
     float spe;
     public float speedStepPerFrame;
     public float MinDistance;
     public Unit target;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start()
+    {
         Physics.IgnoreLayerCollision(12, 11);
 
         Physics.IgnoreLayerCollision(12, 12);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        
+
+    // Update is called once per frame
+    void Update()
+    {
+
         float distance;
 
         Vector3 vdir = (target.transform.position - transform.position);
         distance = vdir.magnitude;
-        if (distance >= MinDistance)
+        if (distance <= MinDistance)
         {
             target.stats.ApplyValue(Unit.StatType.GOLD, 1);
+            Destroy(gameObject);
         }
         vdir.Normalize();
-        transform.position += vdir * startSpeed;
+        transform.position += vdir * startSpeed * distance;
         startSpeed += speedStepPerFrame;
-	}
+    }
 }
