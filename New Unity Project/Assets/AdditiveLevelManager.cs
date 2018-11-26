08f29b830deb10b80e529a70fad2d1d8a147eb78
LastInfantrie <32453888+LastInfantrie@users.noreply.github.com>
2018-11-26 16:09:59 +0100

    nvm just forgot to add the event system again

--- New Unity Project/Assets/AdditiveLevelManager.cs
+++ New Unity Project/Assets/AdditiveLevelManager.cs
@@ -24,7 +24,7 @@ public class AdditiveLevelManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //player = GameObject.FindGameObjectWithTag("Player").GetComponent<Unit>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Unit>();
         UnityEngine.Debug.Assert(instance == null, "Instance singleton is already initialized.");
         instance = this;
         loadedLevels = new Dictionary<int, MapInfo>();
