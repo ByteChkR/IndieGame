eefd75a4e6603f4dad198ff16f794fe559dc8d19
LastInfantrie <32453888+LastInfantrie@users.noreply.github.com>
2018-11-27 17:32:08 +0100

    got started on healthbar can't really test for stuff since Unit and Ability suddenly won't work anymore

---
+++ New Unity Project/Assets/EnemyHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour {
+
+    public Unit Enemy;
+    public Image HealthBar;
+    private float _fill;
+    private float _maxHealth;
+    private float _currentHealth;
+
+	// Use this for initialization
+	void Start () {
+        _maxHealth = Enemy.Stats.MaxHealth;
+	}
+
+	// Update is called once per frame
+	void LateUpdate () {
+        HealthBar.transform.localScale = new Vector3(Enemy.Stats.CurrentHealth / Enemy.Stats.MaxHealth, 1, 1);
+    }
+}
