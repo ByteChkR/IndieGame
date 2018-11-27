bf00b0bc85f5bc9fc114f342970b1ffec01baaa5
LastInfantrie <32453888+LastInfantrie@users.noreply.github.com>
2018-11-27 17:59:43 +0100

    Added boss health bar with inspector testing

--- New Unity Project/Assets/Menu/HUDScript.cs
+++ New Unity Project/Assets/Menu/HUDScript.cs
@@ -11,12 +11,18 @@ public class HUDScript : MonoBehaviour
     public int MaxCombo = 10;
     public float Combo = 0f;
     public int Gold = 0;
-
+    public bool BossBarTest;
+    public float TestBossCurrentHP; 
+    public float TestMaxBossHP;
     public Image HealthBar;
     public Image ComboBar;
+    public Image BossHealthBar;
+    public GameObject BossHealth;
     public Text HealthAmount;
     public Text ComboAmount;
     public Text GoldAmount;
+
+    private Unit _boss;
 
     // Use this for initialization
     void Start()
@@ -59,6 +65,34 @@ public class HUDScript : MonoBehaviour
         GoldAmount.text = "GOLD : " + Gold;
     }
 
+    public void SetBoss(Unit pBoss)
+    {
+        _boss = pBoss;
+    }
+
+    public void RemoveBoss()
+    {
+        _boss = null;
+    }
+
+    private void UpdateBossHealth()
+    {
+        if(_boss != null)
+        {
+            BossHealth.SetActive(true);
+            BossHealthBar.transform.localScale = new Vector3(_boss.Stats.CurrentHealth / _boss.Stats.MaxHealth, 1, 1);
+        }
+        else if(BossBarTest)
+        {
+            BossHealth.SetActive(true);
+            BossHealthBar.transform.localScale = new Vector3(TestBossCurrentHP / TestMaxBossHP, 1, 1);
+        }
+        else
+        {
+            BossHealth.SetActive(false);
+        }
+    }
+
     // Update is called once per frame
     void LateUpdate()
     {
@@ -68,5 +102,6 @@ public class HUDScript : MonoBehaviour
         GoldAmount.text = "GOLD : " + Unit.Player.Stats.CurrentGold;
         HealthBar.transform.localScale = new Vector3(Unit.Player.Stats.CurrentHealth / Unit.Player.Stats.MaxHealth, 1, 1);
         ComboBar.transform.localScale = new Vector3(Unit.Player.Stats.CurrentCombo / Unit.Player.Stats.MaxCombo, 1, 1);
+        UpdateBossHealth();
     }
 }
