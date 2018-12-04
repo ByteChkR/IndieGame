8f9ad05eb8156795e18ac5b493f9b4b119ab5f40
C_Mihai <c_mihaiadrian@yahoo.com>
2018-12-04 21:54:44 +0100

    a lot of stuff

---
+++ New Unity Project/Assets/PlayerCloseLastBoss.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCloseLastBoss : MonoBehaviour {
+
+    public GameObject bossPack;
+    public float range = 16;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Unit.Player == null)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(Unit.Player.transform.position, transform.position)<range)
+            {
+            Instantiate(bossPack, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+		
+	}
+}
